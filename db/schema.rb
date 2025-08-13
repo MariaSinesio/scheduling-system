f3456fd4e8087dbe4b7ac11847b5290b598c91a4
MariaSinesio <marialuizavicentesinesio@gmail.com>
2025-08-13 16:49:19 -0300

    Salvando alterações antes do merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_12_193841) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_13_175536) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_12_193841) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "birth_date"
+    t.string "surname"
+    t.string "gender"
+    t.integer "cpf"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
